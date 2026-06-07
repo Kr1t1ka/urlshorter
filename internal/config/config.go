0d53e39937e6e56132b6d4285471c7abf0813579
p.kukushkin <p.kukushkin@vk.team>
2026-06-07 16:16:57 +0300

    итерация 5

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,20 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"log"
+
+	"github.com/caarlos0/env/v11"
+)
 
 type Config struct {
 	ServerAddr string
 	BaseURL    string
+}
+
+type envVars struct {
+	ServerAddr string `env:"SERVER_ADDRESS"`
+	BaseURL    string `env:"BASE_URL"`
 }
 
 func New() *Config {
@@ -12,5 +22,17 @@ func New() *Config {
 	flag.StringVar(&cfg.ServerAddr, "a", "localhost:8080", "HTTP server address")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Base URL for short links")
 	flag.Parse()
+
+	e := &envVars{}
+	if err := env.Parse(e); err != nil {
+		log.Fatalf("config error: %v", err)
+	}
+	if e.ServerAddr != "" {
+		cfg.ServerAddr = e.ServerAddr
+	}
+	if e.BaseURL != "" {
+		cfg.BaseURL = e.BaseURL
+	}
+
 	return cfg
 }
